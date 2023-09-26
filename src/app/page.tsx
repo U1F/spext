507a71055df776b97efcf539c77842f658520e3b
xintamosaik <xintamosaik@gmail.com>
2023-09-26 23:13:49 +0200

    basic layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,65 +2,65 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main>
-      <menu id="menu">
+    <main className="font-sans flex">
+      <menu id="menu" className="hidden lg:flex basis-96 bg-slate-700">
       <nav>
         <h1>Spex</h1>
 
         <ul>
           <li className="list-item">
-            <i>👉</i>
+            <span>👉</span>
             <span> Dashboard </span>
           </li>
           <li className="list-item">
-            <i>❤️</i>
+            <span>❤️</span>
             <span> Submissions </span>
           </li>
           <li className="list-item">
-            <i>✔️</i>
+            <span>✔️</span>
             <span> Settings </span>
           </li>
           <li className="list-spacer"></li>
           <li className="list-item">
-            <i>🎉</i>
+            <span>🎉</span>
             <span> Music Library </span>
           </li>
           <li className="list-item">
-            <i>⭐</i>
+            <span>⭐</span>
             <span> Artists </span>
           </li>
           <li className="list-spacer"></li>
           <li className="list-item">
-            <i>🫠</i>
+            <span>🫠</span>
             <span> FAQ </span>
           </li>
           <li className="list-item">
-            <i>👍</i>
+            <span>👍</span>
             <span> Contact Us </span>
           </li>
           <li className="list-item">
-            <i>🫶</i>
+            <span>🫶</span>
             <span> About Us </span>
           </li>
           <li className="list-spacer"></li>
           <li className="list-spacer-grow"></li>
           <li className="list-item">
-            <i>💀</i>
+            <span>💀</span>
             <span>Log Out</span>
           </li>
         </ul>
       </nav>
     </menu>
-    <div id="burger">🍔</div>
-    <div id="app">
+    <div id="burger" className="lg:hidden">🍔</div>
+    <div id="app" className="2xl:flex bg-cyan-700 basis-full">
       <div id="main">
         <div id="header">
           <div id="clock"></div>
         </div>
-        <div id="primary" className="soft-shadow">primary</div>
-        <div id="secondary" className="soft-shadow">secondary</div>
-        <div id="additional" className="soft-shadow">additional</div>
-        <div id="misc" className="soft-shadow">misc</div>
+        <div id="primary" className="shadow bg-indigo-700">primary</div>
+        <div id="secondary" className="shadow bg-fuchsia-800">secondary</div>
+        <div id="additional" className="shadow bg-pink-800">additional</div>
+        <div id="misc" className="shadow bg-violet-800">misc</div>
       </div>
 
       <div id="sidebar-slash-footer">sidebar/footer</div>
